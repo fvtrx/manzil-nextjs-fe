39937ae6f4f1d93852a1960d83e7e24ad82df9a3
Abdullah Fitri Che Wan <ftk2431@gmail.com>
2024-02-03 00:27:35 +0800

    First commit for the main screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,136 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
+import { Skeleton } from "@nextui-org/react";
+import Card from "@src/components/common/Card";
+import ContainerBlock from "@src/components/common/ContainerBlock/ContainerBlock";
+import HeroTitle from "@src/components/common/HeroTitle";
+import { BASE_URL, IMeta } from "@src/types";
+import { pushUniqueObject } from "@src/utils/pushUniqueObj";
+import { useRequest } from "@src/utils/useRequest";
 
-const inter = Inter({ subsets: ["latin"] });
+import { useEffect, useState } from "react";
+
+const meta: IMeta = {
+  title: "منزل - Manzil",
+  description:
+    "A list of ayahs that heals and protects against unwanted things",
+  type: "website",
+  image: "/favicon.ico",
+};
+
+interface IAyahAudioItem {
+  id: number;
+  audio_url: string;
+  surahName: string;
+  verseNumber: string;
+}
 
 export default function Home() {
+  const [loading, setLoading] = useState<boolean>(false);
+  const [ayahAudioList, setAyahAudioList] = useState<IAyahAudioItem[]>([]);
+  const { result: surahAlFatihah, run: getSurahAlFatihah } = useRequest({
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/chapter_recitations/7/1`,
+    headers: {
+      Accept: "application/json",
+    },
+  });
+  const { result: surahAlBaqarah, run: getSurahAlBaqarah } = useRequest({
+    method: "get",
+    maxBodyLength: Infinity,
+    url: `${BASE_URL}/quran/recitations/7`,
+    headers: {
+      Accept: "application/json",
+    },
+    params: {
+      chapter_number: 2,
+    },
+  });
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setLoading(true);
+
+      getSurahAlFatihah();
+      getSurahAlBaqarah();
+    }, 3000);
+
+    return () => {
+      clearTimeout(timerId);
+      setLoading(false);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (surahAlFatihah)
+      pushUniqueObject(
+        ayahAudioList,
+        {
+          id: 0,
+          surahName: "Al-Fatihah",
+          audio_url: surahAlFatihah?.audio_file?.audio_url,
+          verseNumber: "1-7",
+        },
+        setAyahAudioList
+      );
+
+    if (surahAlBaqarah) {
+      surahAlBaqarah?.audio_files?.slice(0, 5).map((item: any, index: number) =>
+        pushUniqueObject(
+          ayahAudioList,
+          {
+            id: index + 1,
+            audio_url: `https://verses.quran.com/${item?.url}`,
+            surahName: `Al-Baqarah`,
+            verseNumber: `${index + 1}`,
+          },
+          setAyahAudioList
+        )
+      );
+    }
+  }, []);
+
+  console.log("list of Ayah Audio", ayahAudioList);
+  console.log("this is ayahData", surahAlBaqarah);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+    <ContainerBlock meta={meta}>
+      <div>
+        <HeroTitle
+          title="منزل"
+          subtitle="A list of ayahs that heals and protects against unwanted things."
         />
+
+        <div></div>
+
+        {ayahAudioList.length > 0 ? (
+          <div className="max-w-6xl py-12 grid grid-cols-1 md:grid-cols-3 lg:grid-cols-3 mx-auto place-items-center gap-6">
+            {ayahAudioList?.map(
+              (
+                { audio_url, surahName, verseNumber }: IAyahAudioItem,
+                index
+              ) => (
+                <Skeleton
+                  isLoaded={loading}
+                  className="rounded-xl dark"
+                  classNames={{ base: "bg-slate-500" }}
+                >
+                  <Card
+                    key={index}
+                    index={index}
+                    audioSrc={audio_url}
+                    title={surahName}
+                    description={verseNumber}
+                  />
+                </Skeleton>
+              )
+            )}
+          </div>
+        ) : (
+          <div className="py-12 place-items-center mx-auto justify-center text-center text-xl font-bold">
+            No record found brother..
+          </div>
+        )}
       </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    </ContainerBlock>
   );
 }
