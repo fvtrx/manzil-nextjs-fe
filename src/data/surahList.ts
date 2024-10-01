43b02aa3cc96c2885713554430a6492fb15061cf
Abdullah Fitri Che Wan <ftk2431@gmail.com>
2024-10-01 23:32:18 +0800

    Add translation to each ayat

--- src/data/surahList.ts
+++ src/data/surahList.ts
@@ -47,34 +47,57 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "1-5",
         src: "/ayat/Al-Baqarah-1-5.png",
+        translation: `<h1>Terjemahan</h1>
+
+  <li>Alif, Laam, Miim. (1) </li>
+
+  <li>Kitab Al-Quran ini, tidak ada sebarang syak padanya (tentang datangnya dari Allah dan tentang sempurnanya); ia pula menjadi petunjuk bagi orang-orang yang (hendak) bertaqwa; (2)</li>
+
+  <li>Iaitu orang-orang yang beriman kepada perkara-perkara yang ghaib, dan mendirikan (mengerjakan) sembahyang serta membelanjakan (mendermakan) sebahagian dari rezeki yang Kami berikan kepada mereka. (3)</li>
+
+  <li>Dan juga orang-orang yang beriman kepada Kitab "Al-Quran" yang diturunkan kepadamu (Wahai Muhammad), dan Kitab-kitab yang diturunkan dahulu daripadamu, serta mereka yakin akan (adanya) hari akhirat (dengan sepenuhnya). (4)</li>
+
+  <li>Mereka itulah yang tetap mendapat petunjuk dari Tuhan mereka, dan merekalah orang-orang yang berjaya. (5)</li>
+
+`,
       },
       {
         ayat: "163",
         src: "/ayat/Al-Baqarah-163.png",
+        translation: `<h1>Terjemahan</h1>
+        <p>Dan Tuhan kamu ialah Tuhan yang Maha Esa; tiada Tuhan (Yang berhak disembah) selain dari Allah, yang Maha Pemurah, lagi Maha Mengasihani. (163)</p>
+        `,
       },
       {
         ayat: "255",
         src: "/ayat/Al-Baqarah-255.png",
+        translation: `<h1>Terjemahan</h1> <p>Allah, tiada Tuhan (yang berhak disembah) melainkan Dia, Yang Tetap hidup, Yang Kekal selama-lamanya mentadbirkan (sekalian makhlukNya). Yang tidak mengantuk usahkan tidur. Yang memiliki segala yang ada di langit dan yang ada di bumi. Tiada sesiapa yang dapat memberi syafaat (pertolongan) di sisiNya melainkan dengan izinNya. yang mengetahui apa yang ada di hadapan mereka dan apa yang ada di belakang mereka, sedang mereka tidak mengetahui sesuatu pun dari (kandungan) ilmu Allah melainkan apa yang Allah kehendaki (memberitahu kepadanya). Luasnya Kursi Allah (ilmuNya dan kekuasaanNya) meliputi langit dan bumi; dan tiadalah menjadi keberatan kepada Allah menjaga serta memelihara keduanya. Dan Dia lah Yang Maha Tinggi (darjat kemuliaanNya), lagi Maha Besar (kekuasaanNya). (255)</p>
+        `,
       },
       {
         ayat: "256",
         src: "/ayat/Al-Baqarah-256.png",
+        translation: `<h1>Terjemahan</h1> <p>Tidak ada paksaan dalam ugama (Islam), kerana sesungguhnya telah nyata kebenaran (Islam) dari kesesatan (kufur). Oleh itu, sesiapa yang tidak percayakan Taghut, dan ia pula beriman kepada Allah, maka sesungguhnya ia telah berpegang kepada simpulan (tali ugama) yang teguh yang tidak akan putus. Dan (ingatlah), Allah Maha Mendengar, lagi Maha Mengetahui. (256)</p>`,
       },
       {
         ayat: "257",
         src: "/ayat/Al-Baqarah-257.png",
+        translation: `<h1>Terjemahan</h1> <p>Allah Pelindung (Yang mengawal dan menolong) orang-orang yang beriman. Ia mengeluarkan mereka dari kegelapan (kufur) kepada cahaya (iman). Dan orang-orang yang kafir, penolong-penolong mereka ialah Taghut yang mengeluarkan mereka dari cahaya (iman) kepada kegelapan (kufur). Mereka itulah ahli neraka, mereka kekal di dalamnya. (257)</p>`,
       },
       {
         ayat: "284",
         src: "/ayat/Al-Baqarah-284.png",
+        translation: `<h1>Terjemahan</h1> <p>Segala yang ada di langit dan yang ada di bumi adalah kepunyaan Allah. Dan jika kamu melahirkan apa yang ada di dalam hati kamu atau kamu memyembunyikannya, nescaya Allah akan menghitung dan menyatakannya kepada kamu. Kemudian Ia mengampunkan bagi sesiapa yang dikehendakiNya dan menyeksa sesiapa yang dikehendakiNya (menurut undang-undang peraturanNya) Dan (ingatlah), Allah Maha Kuasa atas tiap-tiap sesuatu. (284)</p>`,
       },
       {
         ayat: "285",
         src: "/ayat/Al-Baqarah-285.png",
+        translation: `<h1>Terjemahan</h1> <p>Rasulullah telah beriman kepada apa yang diturunkan kepadanya dari Tuhannya, dan juga orang-orang yang beriman; semuanya beriman kepada Allah, dan Malaikat-malaikatNya, dan Kitab-kitabNya, dan Rasul-rasulNya. (Mereka berkata): "Kami tidak membezakan antara seorang dengan yang lain Rasul-rasulnya". Mereka berkata lagi: Kami dengar dan kami taat (kami pohonkan) keampunanMu wahai Tuhan kami, dan kepadaMu jualah tempat kembali". (285)</p>`,
       },
       {
         ayat: "286",
         src: "/ayat/Al-Baqarah-286.png",
+        translation: `<h1>Terjemahan</h1> <p>Allah tidak memberati seseorang melainkan apa yang terdaya olehnya. Ia mendapat pahala kebaikan yang diusahakannya, dan ia juga menanggung dosa kejahatan yang diusahakannya. (Mereka berdoa dengan berkata): "Wahai Tuhan kami! Janganlah Engkau mengirakan kami salah jika kami lupa atau kami tersalah. Wahai Tuhan kami ! Janganlah Engkau bebankan kepada kami bebanan yang berat sebagaimana yang telah Engkau bebankan kepada orang-orang yang terdahulu daripada kami. Wahai Tuhan kami! Janganlah Engkau pikulkan kepada kami apa yang kami tidak terdaya memikulnya. Dan maafkanlah kesalahan kami, serta ampunkanlah dosa kami, dan berilah rahmat kepada kami. Engkaulah Penolong kami; oleh itu, tolonglah kami untuk mencapai kemenangan terhadap kaum-kaum yang kafir". (286)</p>`,
       },
     ],
   },
@@ -86,6 +109,7 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "18",
         src: "/ayat/Al-Imran-18.png",
+        translation: `<h1>Terjemahan</h1> <p>Allah menerangkan (kepada sekalian makhlukNya dengan dalil-dalil dan bukti), bahawasanya tiada Tuhan (yang berhak disembah) melainkan Dia, Yang sentiasa mentadbirkan (seluruh alam) dengan keadilan, dan malaikat-malaikat serta orang-orang yang berilmu (mengakui dan menegaskan juga yang demikian); tiada Tuhan (yang berhak disembah) melainkan Dia; Yang Maha Kuasa, lagi Maha Bijaksana. (18)</p>`,
       },
     ],
   },
@@ -97,14 +121,17 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "116",
         src: "/ayat/Al-Mukminun-116.png",
+        translation: `<h1>Terjemahan</h1> <p>Maka (dengan yang demikian) Maha Tinggilah Allah Yang Menguasai seluruh alam, lagi Yang Tetap Benar; tiada Tuhan melainkan Dia, Tuhan yang mempunyai Arasy yang mulia. (116)</p>`,
       },
       {
         ayat: "117",
         src: "/ayat/Al-Mukminun-117.png",
+        translation: `<h1>Terjemahan</h1> <p>Dan sesiapa yang menyembah tuhan yang lain bersama-sama Allah, dengan tidak berdasarkan sebarang keterangan mengenainya, maka sesungguhnya hitungannya (dan balasan amalnya yang jahat itu) disediakan di sisi Tuhannya. Sesungguhnya orang-orang yang kafir tidak akan berjaya. (117)</p>`,
       },
       {
         ayat: "118",
         src: "/ayat/Al-Mukminun-118.png",
+        translation: `<h1>Terjemahan</h1> <p>Dan berdoalah (wahai Muhammad dengan berkata): "Wahai Tuhanku, berikanlah ampun dan kurniakan rahmat, dan sememangnya Engkaulah sahaja sebaik-baik Pemberi rahmat!" (118)</p>`,
       },
     ],
   },
@@ -116,42 +143,52 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "1",
         src: "/ayat/As-Saffat-1.png",
+        translation: `<h1>Terjemahan</h1> <p>Demi (hamba-hambaKu) yang berbaris dengan berderet-deret - (1)</p>`,
       },
       {
         ayat: "2",
         src: "/ayat/As-Saffat-2.png",
+        translation: `<h1>Terjemahan</h1> <p>(Hamba-hambaKu) yang melarang (dari kejahatan) dengan sesungguh-sungguhnya - (2)</p>`,
       },
       {
         ayat: "3",
         src: "/ayat/As-Saffat-3.png",
+        translation: `<h1>Terjemahan</h1> <p>(Hamba-hambaKu) yang membaca kandungan Kitab Suci; (3)</p>`,
       },
       {
         ayat: "4",
         src: "/ayat/As-Saffat-4.png",
+        translation: `<h1>Terjemahan</h1> <p>(Sumpah demi sumpah) sesungguhnya Tuhan kamu hanyalah Satu - (4)</p>`,
       },
       {
         ayat: "5",
         src: "/ayat/As-Saffat-5.png",
+        translation: `<h1>Terjemahan</h1> <p>Tuhan (yang mencipta serta mentadbirkan) langit dan bumi dan segala yang ada di antara keduanya, dan Tuhan (yang mengatur) tempat-tempat terbit matahari. (5)</p>`,
       },
       {
         ayat: "6",
         src: "/ayat/As-Saffat-6.png",
+        translation: `<h1>Terjemahan</h1> <p>Sesungguhnya Kami telah menghiasi langit yang dekat (pada penglihatan penduduk bumi) dengan hiasan bintang-bintang. (6)</p>`,
       },
       {
         ayat: "7",
         src: "/ayat/As-Saffat-7.png",
+        translation: `<h1>Terjemahan</h1> <p>Dan (Kami pelihara urusan langit itu) dengan serapi-rapi kawalan dari (masuk campur) tiap-tiap Syaitan yang derhaka; (7)</p>`,
       },
       {
         ayat: "8",
         src: "/ayat/As-Saffat-8.png",
+        translation: `<h1>Terjemahan</h1> <p>(Dengan itu) mereka tidak dapat memasang telinga mendengar (percakapan malaikat) penduduk langit, dan mereka pula direjam (dengan api) dari segala arah dan penjuru, (8)</p>`,
       },
       {
         ayat: "9",
         src: "/ayat/As-Saffat-9.png",
+        translation: `<h1>Terjemahan</h1> <p>Untuk mengusir mereka; dan mereka pula beroleh azab seksa yang tidak putus-putus. (9)</p>`,
       },
       {
         ayat: "10",
         src: "/ayat/As-Saffat-10.png",
+        translation: `<h1>Terjemahan</h1> <p>Kecuali sesiapa di antara Syaitan-syaitan itu yang curi mendengar mana-mana percakapan (malaikat), maka ia diburu dan diikuti (dengan rejaman) api yang menjulang lagi menembusi. (10)</p>`,
       },
     ],
   },
@@ -163,6 +200,14 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "22-24",
         src: "/ayat/Al-Hasyr.png",
+        translation: `<h1>Terjemahan</h1>
+
+  <li>Dia lah Allah, yang tidak ada Tuhan melainkan Dia; Yang Mengetahui perkara yang ghaib dan yang nyata; Dia lah Yang Maha Pemurah, lagi Maha Mengasihani. (22) </li>
+
+  <li>Dia lah Allah, yang tidak ada Tuhan melainkan Dia; Yang Menguasai (sekalian alam); Yang Maha Suci; Yang Maha Selamat Sejahtera (dari segala kekurangan); Yang Maha Melimpahkan Keamanan; Yang Maha Pengawal serta Pengawas; Yang Maha Kuasa; Yang Maha Kuat (menundukkan segala-galanya); Yang Melengkapi segala KebesaranNya. Maha Suci Allah dari segala yang mereka sekutukan denganNya. (23)</li>
+
+  <li>Dia lah Allah, Yang Menciptakan sekalian makhluk; Yang Mengadakan (dari tiada kepada ada); Yang Membentuk rupa (makhluk-makhlukNya menurut yang dikehendakiNya); bagiNyalah nama-nama yang sebaik-baiknya dan semulia-mulianya; bertasbih kepadaNya segala yang ada di langit dan di bumi; dan Dia lah Yang tiada bandingNya, lagi Maha Bijaksana. (24)</li>
+`,
       },
     ],
   },
@@ -174,6 +219,7 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "3",
         src: "/ayat/Al-Jinn-3.png",
+        translation: `<h1>Terjemahan</h1> <p>Dan (ketahuilah wahai kaum kami!) Bahawa sesungguhnya: tertinggilah kebesaran dan keagungan Tuhan kita daripada beristeri atau beranak. (3)</p>`,
       },
     ],
   },
@@ -185,6 +231,16 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "1-4",
         src: "/ayat/Al-Ikhlas.png",
+        translation: `<h1>Terjemahan</h1>
+
+  <li>Katakanlah (wahai Muhammad): "(Tuhanku) ialah Allah Yang Maha Esa; (1)</li>
+
+  <li>Allah Yang menjadi tumpuan sekalian makhluk untuk memohon sebarang hajat; (2)</li>
+
+  <li>Ia tiada beranak, dan Ia pula tidak diperanakkan; (3)</li>
+
+  <li>"Dan tidak ada sesiapapun yang serupa denganNya". (4)</li>
+`,
       },
     ],
   },
@@ -196,6 +252,18 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "1-5",
         src: "/ayat/Al-Falaq.png",
+        translation: `<h1>Terjemahan</h1>
+
+  <li>Katakanlah (wahai Muhammad); "Aku berlindung kepada (Allah) Tuhan yang menciptakan sekalian makhluk, (1)</li>
+
+  <li>Dari bencana makhluk-makhluk yang Ia ciptakan; (2)</li>
+
+  <li>Dan dari bahaya gelap apabila ia masuk; (3)</li>
+
+  <li>Dan dari kejahatan makhluk-makhluk yang menghembus-hembus pada simpulan-simpulan (dan ikatan-ikatan);. (4)</li>
+
+  <li>"Dan dari kejahatan orang yang dengki apabila ia melakukan dengkinya". (5)</li>
+  `,
       },
     ],
   },
@@ -207,6 +275,20 @@ export const surahList: ISurahItem[] = [
       {
         ayat: "1-6",
         src: "/ayat/An-Nas.png",
+        translation: `<h1>Terjemahan</h1>
+
+  <li>Katakanlah (wahai Muhammad): "Aku berlindung kepada (Allah) Pemulihara sekalian manusia." (1)</li>
+
+  <li>Yang Menguasai sekalian manusia, (2)</li>
+
+  <li>Tuhan yang berhak disembah oleh sekalian manusia, (3)</li>
+
+  <li>Dari kejahatan pembisik penghasut yang timbul tenggelam, - (4)</li>
+
+  <li>Yang melemparkan bisikan dan hasutannya ke dalam hati manusia, -. (5)</li>
+
+  <li>"(Iaitu pembisik dan penghasut) dari kalangan jin dan manusia". (6)</li>
+  `,
       },
     ],
   },
